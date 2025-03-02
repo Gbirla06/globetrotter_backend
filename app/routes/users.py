a83a451e33f20979a2c7a034511f881a8e9898f1
Ganesh Birla <ganesh.birla@vengage.ai>
2025-03-02 10:47:05 +0530

    Commented unnecessory code

--- app/routes/users.py
+++ app/routes/users.py
@@ -32,30 +32,30 @@ async def register_user(username : str) :
     return {'username' : username, 'score' : 0, 'correct_score' : 0, 'incorrect_score' : 0, 'total_referrals' : 0} 
 
 
-@router.post("/challenge")
-async def challenge_friend(challenger_username: str, friend_username: str):
-    challenger_username = challenger_user.lower()
-    friend_username = friend_username.lower()
-    challenger_user = await users_collection.find_one({'username' : challenger_username})
+# @router.post("/challenge")
+# async def challenge_friend(challenger_username: str, friend_username: str):
+#     challenger_username = challenger_user.lower()
+#     friend_username = friend_username.lower()
+#     challenger_user = await users_collection.find_one({'username' : challenger_username})
 
-    if challenger_user is None :
-        raise HTTPException(status_code=404, detail="Challenger not found")
+#     if challenger_user is None :
+#         raise HTTPException(status_code=404, detail="Challenger not found")
     
-    friend_user = await users_collection.find_one({'username' : friend_username})
+#     friend_user = await users_collection.find_one({'username' : friend_username})
 
-    if friend_user :
-        raise HTTPException(status_code=404, detail="User already exist")
+#     if friend_user :
+#         raise HTTPException(status_code=404, detail="User already exist")
     
-    challenge_entry = {
-        "friend" : friend_username,
-        "timestamp" : datetime.now()
-    }
+#     challenge_entry = {
+#         "friend" : friend_username,
+#         "timestamp" : datetime.now()
+#     }
 
-    users_collection.update_one(
-        {"username" : challenger_username},
-        {"$push" : {"challenges" : challenge_entry}}
-    )
+#     users_collection.update_one(
+#         {"username" : challenger_username},
+#         {"$push" : {"challenges" : challenge_entry}}
+#     )
 
-    share_link = f"{APPLICATION_URL}/play?challenge_id={friend_username}"
+#     share_link = f"{APPLICATION_URL}/play?challenge_id={friend_username}"
 
-    return {"message": "Challenge sent!", "share_link": share_link}
+#     return {"message": "Challenge sent!", "share_link": share_link}
